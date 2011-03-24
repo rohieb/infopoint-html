17e204cbbe3add88316c8d63e83d08556cff6efd
Roland Hieber <rohieb@rohieb.name>
2011-03-24 01:34:35 +0100

    new XULRunner version of vertretungsplan-anzeige

---
+++ src/defaults/preferences/prefs.js
@@ -0,0 +1,4 @@
+pref("toolkit.defaultChromeURI", "chrome://infopointhtmlviewer/content/main.xul");
+pref("toolkit.singletonWindowType", "viewer");
+pref("toolkit.defaultChromeFeatures", "dialog=no");
+
