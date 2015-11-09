8145d8713441acd6f09c22392bf4a5e683fc802a
Roland Hieber <rohieb@rohieb.name>
2015-11-09 01:21:53 +0100

    rename from infopoint-html-viewer to infopointhtml

--- src/xul/defaults/preferences/prefs.js
+++ src/xul/defaults/preferences/prefs.js
@@ -1,4 +1,4 @@
-pref("toolkit.defaultChromeURI", "chrome://infopointhtmlviewer/content/main.xul");
+pref("toolkit.defaultChromeURI", "chrome://infopointhtml/content/main.xul");
 pref("toolkit.singletonWindowType", "viewer");
 pref("toolkit.defaultChromeFeatures", "dialog=no");
 
